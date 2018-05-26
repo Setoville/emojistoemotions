c300c06fcaf7b20b0eac30f49719bae363999266
andrew <andrewseto@live.com>
2018-05-26 01:13:59 -0400

    Adding schema, model and query

--- emojistoemotionsapi/functions/__main__.js
+++ emojistoemotionsapi/functions/__main__.js
@@ -1,10 +1,39 @@
-/**
-* A basic Hello World function
-* @param {string} name Who you're saying hello to
-* @returns {string}
-*/
-module.exports = (name = 'world', context, callback) => {
+const lib = require('lib');
+const mongoose = require('mongoose');
 
-  callback(null, `hello ${name}`);
+/**
+* @param {string} unicode
+* @returns {any}
+*/
+
+
+module.exports = async (unicode, context) => {
+  var emojiSchema = mongoose.Schema({
+    "unicode": String,
+    "nickname": String,
+    "acousticness": String,
+    "danceability": Number,
+    "duration_ms": String,
+    "energy": Number,
+    "instrumentalness": String,
+    "key": String,
+    "liveness": Number,
+    "loudness": Number,
+    "mode": Number,
+    "popularity": Number,
+    "speechiness": String,
+    "tempo": String,
+    "time_signature": String,
+    "valence": Number
+  });
+
+  var emoji = mongoose.model('emoji', emojiSchema);
+
+  var url = "mongodb://seto:123@ds235860.mlab.com:35860/emojistoemotionsdatabase";
+
+  await mongoose.connect(url);
+  emoji.find({
+      "unicode": unicode
+  });
 
 };
